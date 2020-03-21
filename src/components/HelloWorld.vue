c5a8b5f29d5a0fb8ae4dbc71954a1f7e67d67f5c
David <tdavidjin@gmail.com>
2020-03-21 16:36:12 +0100

    Update HelloWorld.vue

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,28 +1,52 @@
 <template>
-  <div class="hello">
+
+  <div class="bg">
+    
     <h3>{{ msg }}</h3>
     <p class="mb-2">Hiermit wette ich  <input type="text" id="name1" class="form-control" placeholder="Dein Name"> mit  <input type="text" id="name2" class="form-control" placeholder ="Wettpartner"> Die Wette lautet wie folgend</p>
     <input type="text" id="text_groß" class="form-control">
     <p> Der Wetteinsatz ist der folgende: </p>
     <input type="text" id="text_groß" class="form-control">
     <mdb-btn outline="primary">Bestätigen</mdb-btn>
+    
     </div>
+    
 </template>
   
 <script>
- //hello
+import {mdbBtn} from 'mdbvue';
 export default {
-  name: 'HelloWorld',
-  data () {
-    return {
-      msg: 'Vertrag'
-    };
+  name: 'ButtonPage',
+  components: {
+    mdbBtn
   }
+
 }
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
+  body, html {
+    height: 100%;
+  }
+  .bg {
+    /* The image used */
+    background-color:ghostwhite;
+
+    /* Full height */
+    height: 100%;
+    width:50%;
+    margin:auto;
+  
+    ;
+
+    /* Center and scale the image nicely */
+    background-position: center;
+    background-repeat: no-repeat;
+    background-size: cover;
+  }
+
+
 h3 {
   font-weight: normal;
   padding-top: 20px;
