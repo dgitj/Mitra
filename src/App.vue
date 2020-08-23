fa7b6a288f46e03eb465fd06c4efceaee9f41b7a
David <tdavidjin@gmail.com>
2020-08-23 14:30:37 +0200

    tweets

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,8 @@
 <template>
   <div id="app">
     <!--img id="handshake-img" src="./assets/handshake-transparent.png" data-html2canvas-ignore-->
+    <router-link to ="tweets">Tweets </router-link>
+    <router-link to ="bet"> bet</router-link>
     <router-view/>
   </div>
 </template>
@@ -16,29 +18,4 @@ export default {
 <style>
   @import url('https://fonts.googleapis.com/css?family=Roboto:300,400,500,700&display=swap');
   @import url("https://fonts.googleapis.com/css?family=Lora:400,700|Montserrat:300");
-
-  /*html, body {
-    height: 100%;
-  }
-  */
-  
-  /*
-  #app {
-    text-align: center;
-    margin-top: 0px;
-    height: auto;
-    width: auto;
-    min-height: 900px;
-    background-image: url("https://mdbootstrap.com/img/Photos/Horizontal/Nature/full page/img(11).jpg");
-    background-position: center;
-    background-repeat: no-repeat;
-    background-size: cover;
-    background: auto;
-  }
-
-*/
-  #handshake-img {
-    height: 100px;
-    opacity: 0.5;
-  }
 </style>
--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,8 @@
 import Vue from 'vue';
 import Router from 'vue-router';
 import HelloWorld from '@/components/HelloWorld';
+import tweets from "@/components/tweets";
+import bet from "@/components/bet";
 
 Vue.use(Router);
 
@@ -10,6 +12,18 @@ export default new Router({
       path: '/',
       name: 'HelloWorld',
       component: HelloWorld
+    },
+
+    {
+      path: '/bet',
+      name: 'bet',
+      component: bet
+    },
+
+    {
+      path: "/tweets",
+      name: "tweets",
+      component: tweets
     }
   ]
 });
